81f772c915a4f86b960686d9ebf10c9d7a591a90
Christopher Neely <christopher.neely1200@gmail.com>
2020-07-09 18:46:09 -0700

    Still working on debug

--- EukMetaSanity/src/tasks/taxonomy.py
+++ EukMetaSanity/src/tasks/taxonomy.py
@@ -18,13 +18,14 @@ class TaxonomyIter(TaskList):
                 self.input[0],  # Input FASTA sequence for repeat masking
                 seq_db,  # MMseqs database for use in metaeuk or repeat masking
             ]
-            # Data to pass forward
-            self.passed_data["tax_assignment"], self.passed_data["tax_id"] = TaxonomyIter.Taxonomy.get_taxonomy(
-                os.path.join(self.wdir, "tax-report.txt"), float(self.cutoff)
-            )
+            if os.path.exists(os.path.join(self.wdir, "tax-report.txt")):
+                self.passed_data["tax_assignment"], self.passed_data["tax_id"] = TaxonomyIter.Taxonomy.get_taxonomy(
+                    os.path.join(self.wdir, "tax-report.txt"), float(self.cutoff)
+                )
 
         def run(self) -> None:
             super().run()
+            # Data to pass forward
 
         @program_catch
         def run_1(self):
@@ -61,6 +62,9 @@ class TaxonomyIter(TaskList):
                     tax_report
                 ]
             )
+            self.passed_data["tax_assignment"], self.passed_data["tax_id"] = TaxonomyIter.Taxonomy.get_taxonomy(
+                os.path.join(self.wdir, "tax-report.txt"), float(self.cutoff)
+            )
 
         @staticmethod
         def get_taxonomy(tax_results_file: str, cutoff: float) -> Tuple[str, int]:
@@ -68,24 +72,23 @@ class TaxonomyIter(TaskList):
             _id: int = 2759
             if not os.path.exists(tax_results_file):
                 return assignment.lower(), _id
-            # Get first line
-            _tax_results_file = open(tax_results_file, "r")
             try:
+                # Get first line
+                _tax_results_file = open(tax_results_file, "r")
                 while True:
                     line = next(_tax_results_file).rstrip("\r\n").split("\t")
                     # Parse line for assignment
-                    _score, _tax_id, _assignment = float(line[0]), line[4], line[5].lstrip(" ")
+                    _score, _tax_id, _assignment = float(line[0]), int(line[4]), line[5].lstrip(" ")
                     if _assignment in ("unclassified", "root"):
                         continue
-                    if _score < cutoff:
-                        break
-                    # Keep new value if >= cutoff% of contigs map to the taxonomy
-                    else:
+                    if _score >= cutoff:
                         assignment = _assignment
                         _id = _tax_id
+                    if _score < cutoff:
+                        return assignment.lower(), _id
+            except:
                 return assignment.lower(), _id
-            except Exception:
-                return assignment.lower(), _id
+            return assignment.lower(), _id
 
     def __init__(self, *args, **kwargs):
         super().__init__(TaxonomyIter.Taxonomy, "taxonomy", *args, **kwargs)
