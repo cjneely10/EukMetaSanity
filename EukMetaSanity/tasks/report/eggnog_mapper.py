5366c42624b951b0d018beae38faa0e60f2f1aaa
Christopher Neely <christopher.neely1200@gmail.com>
2020-07-18 14:28:54 -0700

    Finish eggnog mapper

--- EukMetaSanity/tasks/report/eggnog_mapper.py
+++ EukMetaSanity/tasks/report/eggnog_mapper.py
@@ -6,13 +6,25 @@ class EggNOGMapper(TaskList):
     class EggNOG(Task):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
+            self.output = [
+                *self.input,  # Forward values
+                os.path.join(self.wdir, self.record_id + ".emapper.annotations")  # Results of mapper
+            ]
 
         def run(self):
             super().run()
 
         @program_catch
         def run_1(self):
-            pass
+            self.log_and_run(
+                self.program_python27[
+                    self.emapper,
+                    "-i", self.input[0],
+                    "--output", self.output[-1],
+                    "--cpu", self.threads,
+                    *self.added_flags,
+                ]
+            )
 
     def __init__(self, *args, **kwargs):
         super().__init__(EggNOGMapper.EggNOG, "eggnog", *args, **kwargs)
