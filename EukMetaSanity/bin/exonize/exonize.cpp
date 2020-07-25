7ade63700415018f1728498a4ecf7f7e55edf849
Christopher Neely <christopher.neely1200@gmail.com>
2020-07-25 12:49:35 -0700

    Begin exonize c++ logic

---
+++ EukMetaSanity/bin/exonize/exonize.cpp
@@ -0,0 +1,14 @@
+#include <cstring>
+#include <string>
+
+/// Helper function declarations
+
+std::string get_output_file(int*, char*[]);
+std::string get_input_file(int*, char*[]);
+
+
+/// Main program logic
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
