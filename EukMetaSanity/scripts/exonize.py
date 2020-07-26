5c233dea864f96fe83f2996b07009536df487f81
Christopher Neely <christopher.neely1200@gmail.com>
2020-07-25 21:27:05 -0700

    Continue through exonize - need to think of means to merge intervals

--- EukMetaSanity/scripts/exonize.py
+++ EukMetaSanity/scripts/exonize.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import os
 from Bio import SeqIO
+from typing import List
+from Bio.SeqRecord import SeqRecord
+from collections import defaultdict
 from EukMetaSanity.utils.arg_parse import ArgParse
+
+init_mapping = {
+    **{key: 0 for key in ("A", "C", "G", "T")},
+    "N": 1,
+}
 
 
 def _parse_args(ap: ArgParse):
@@ -11,14 +19,30 @@ def _parse_args(ap: ArgParse):
 
 def exonize(fasta_file: str, gff3_file: str, output_file: str):
     w = open(output_file, "w")
-    gff3_fp = open(gff3_file, "r")
+    _gff3_fp = open(gff3_file, "r")
     seq_dict = SeqIO.to_dict(SeqIO.parse(fasta_file, "fasta"))
-    w.write("##gff-version 3\n")
-    for line in gff3_fp:
-        if line.startswith("#"):
-            continue
-        line = line.rstrip("\r\n").split("\t")
+    _data = defaultdict(list)
+    parse_data(_data, _gff3_fp)
+    _out_data = defaultdict(list)
 
+
+def parse_data(data, gff3_fp):
+    for _line in gff3_fp:
+        if _line.startswith("#"):
+            continue
+        line = _line.rstrip("\r\n").split("\t")
+        data[line[0]].append((line[2], int(line[3]), int(line[4]), line[6]))
+
+
+def flatten(data, out_data):
+    for _id, feature_list in data.items():
+        for feature_tuple in feature_list:
+            pass
+
+
+def generate_initial_region(record: SeqRecord, _region: List[int]):
+    for i, val in enumerate(record.seq):
+        _region[i] = init_mapping[val]
 
 
 if __name__ == "__main__":
@@ -28,7 +52,7 @@ if __name__ == "__main__":
              {"help": "Input FASTA file", "required": True}),
             (("-g", "--gff3_file"),
              {"help": "Input GFF3 file", "required": True}),
-            (("-o", "--output"),
+            (("-o", "--output_file"),
              {"help": "Output path, default stdout", "default": "/dev/stdout"}),
         ),
         description="Convert EukMetaSanity .merged.gff3 into exonized .nr.gff3 file"
