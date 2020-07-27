5bb24c0e68362d73e9212b4d655de430c7735a1d
Christopher Neely <christopher.neely1200@gmail.com>
2020-07-26 17:03:32 -0700

    Continue exonize script with brute force method

--- EukMetaSanity/scripts/exonize.py
+++ EukMetaSanity/scripts/exonize.py
@@ -1,48 +1,91 @@
 #!/usr/bin/env python3
 import os
 from Bio import SeqIO
-from typing import List
+from typing import List, Dict
+from collections import namedtuple
 from Bio.SeqRecord import SeqRecord
 from collections import defaultdict
 from EukMetaSanity.utils.arg_parse import ArgParse
 
-init_mapping = {
-    **{key: 0 for key in ("A", "C", "G", "T")},
-    "N": 1,
-}
+GFFCoord = namedtuple("GFFCoord", ("evidence", "loc_type", "start", "end", "strand", "parent_id"))
 
 
-def _parse_args(ap: ArgParse):
-    for _path in (ap.args.fasta_file, ap.args.gff3_file):
-        assert os.path.exists(_path)
+class Coordinate:
+    __slots__ = (
+        "is_repeat_region",
+        "is_exon",
+        "is_intron",
+        "parent_id",
+        "evidence",
+        "strand",
+        "nucleotide",
+    )
+
+    def __init__(self, nucleotide: str):
+        if nucleotide == "N":
+            self.is_repeat_region = True
+        else:
+            self.is_repeat_region = False
+        self.is_exon = False
+        self.is_intron = False
+        self.parent_id = None
+        self.evidence = []
+        self.strand = None
+        self.nucleotide = nucleotide
 
 
-def exonize(fasta_file: str, gff3_file: str, output_file: str):
-    w = open(output_file, "w")
-    _gff3_fp = open(gff3_file, "r")
-    seq_dict = SeqIO.to_dict(SeqIO.parse(fasta_file, "fasta"))
-    _data = defaultdict(list)
-    parse_data(_data, _gff3_fp)
-    _out_data = defaultdict(list)
-
-
-def parse_data(data, gff3_fp):
+# Convert gff3 file to dictionary
+def gff3_to_dict(gff3_file: str) -> Dict[str, List[GFFCoord]]:
+    gff3_fp = open(gff3_file, "r")
+    out_data = defaultdict(list)
     for _line in gff3_fp:
         if _line.startswith("#"):
             continue
-        line = _line.rstrip("\r\n").split("\t")
-        data[line[0]].append((line[2], int(line[3]), int(line[4]), line[6]))
+        line = _line.rstrip("\r\n").split()
+        out_data[line[0]].append(
+            GFFCoord(
+                evidence=line[1],
+                loc_type=line[2],
+                start=int(line[3]),
+                end=int(line[4]),
+                strand=line[6],
+                parent_id=line[8].split(";")[0].split("=")[1]
+            )
+        )
+    return dict(out_data)
 
 
-def flatten(data, out_data):
-    for _id, feature_list in data.items():
-        for feature_tuple in feature_list:
+# Create list of Coordinate objects that describe each location on a chromosome
+def generate_initial_region(record: SeqRecord) -> List[Coordinate]:
+    return [
+        Coordinate(val)
+        for val in record.seq
+    ]
+
+
+# # Program driver logic
+
+def _parse_args(ap: ArgParse):
+    for _path in (ap.args.fasta_file, *ap.args.gff3_files):
+        assert os.path.exists(_path)
+
+
+def exonize(fasta_file: str, gff3_files: List[str], output_file: str):
+    w = open(output_file, "w")
+    # Get FASTA file as dict
+    record_p = SeqIO.parse(fasta_file, "fasta")
+    # Generate list of GFF data dictionaries
+    gff_dict_list = [
+        gff3_to_dict(_file) for _file in gff3_files
+    ]
+    # Iterate over each record
+    for record in record_p:
+        # Create bare region
+        region = generate_initial_region(record)
+        for gff_dict in gff_dict_list:
             pass
 
-
-def generate_initial_region(record: SeqRecord, _region: List[int]):
-    for i, val in enumerate(record.seq):
-        _region[i] = init_mapping[val]
+    w.close()
 
 
 if __name__ == "__main__":
@@ -50,12 +93,12 @@ if __name__ == "__main__":
         (
             (("-f", "--fasta_file"),
              {"help": "Input FASTA file", "required": True}),
-            (("-g", "--gff3_file"),
-             {"help": "Input GFF3 file", "required": True}),
+            (("-g", "--gff3_files"),
+             {"help": "Input GFF3 files", "required": True, "nargs": "+"}),
             (("-o", "--output_file"),
              {"help": "Output path, default stdout", "default": "/dev/stdout"}),
         ),
         description="Convert EukMetaSanity .merged.gff3 into exonized .nr.gff3 file"
     )
     _parse_args(_ap)
-    exonize(_ap.args.fasta_file, _ap.args.gff3_file, _ap.args.output_file)
+    exonize(_ap.args.fasta_file, _ap.args.gff3_files, _ap.args.output_file)
