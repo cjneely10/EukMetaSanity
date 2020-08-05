f0e8ea5150da53c9de673cf48d17520b3b180902
Christopher Neely <christopher.neely1200@gmail.com>
2020-08-04 17:44:06 -0700

    Finish align step using minimap2 and sambamba

--- EukMetaSanity/tasks/refine/align.py
+++ EukMetaSanity/tasks/refine/align.py
@@ -8,7 +8,7 @@ class AlignIter(TaskList):
             super().__init__(*args, **kwargs)
             self.output = [
                 *self.input,
-                os.path.join(self.wdir, self.record_id + ".bam"),  # Sorted bam file
+                os.path.join(self.wdir, self.record_id + ".sorted.bam"),  # Sorted bam file
             ]
 
         def run(self) -> None:
@@ -16,12 +16,15 @@ class AlignIter(TaskList):
 
         @program_catch
         def run_1(self):
+            # Align to genome
             self.log_and_run(
                 self.program_minimap2[
                     "-ax", self.minimap_option,
                     self.input[4], self.input[-2], self.input[-1],
+                    (*self.added_flags),
                 ] > os.path.join(self.wdir, self.record_id + ".sam")
             )
+            # Convert to bam
             self.log_and_run(
                 self.program_sambamba[
                     "view",
@@ -30,15 +33,19 @@ class AlignIter(TaskList):
                     "-t", self.threads,
                 ]
             )
+            # Sort
             self.log_and_run(
                 self.program_sambamba[
                     "sort",
-                    "--format=bam", "-o", os.path.join(self.wdir, self.record_id + ".bam"),
+                    "--format=bam", "-o", os.path.join(self.wdir, self.record_id + ".sorted.bam"),
                     "--tmpdir=%s" % os.path.join(self.wdir, "tmp"),
                     "-t", self.threads, "-m", self.memory_limit,
                     os.path.join(self.wdir, self.record_id + ".bam"),
                 ]
             )
+            # Remove intermediaries
+            self.local["rm"][os.path.join(self.wdir, self.record_id + ".bam")]()
+            self.local["rm"][os.path.join(self.wdir, self.record_id + ".sam")]()
 
     def __init__(self, *args, **kwargs):
         super().__init__(AlignIter.Align, "align", *args, **kwargs)
