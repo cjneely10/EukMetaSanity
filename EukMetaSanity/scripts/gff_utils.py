2c1176867e3274f67e0cdf5010875f1a4accd51d
Christopher Neely <christopher.neely1200@gmail.com>
2020-08-25 18:26:12 -0700

    Begin new annotation scheme

---
+++ EukMetaSanity/scripts/gff_utils.py
@@ -0,0 +1,17 @@
+from EukMetaSanity.utils.arg_parse import ArgParse
+
+
+class GffReader:
+    pass
+
+
+class AnnotationIter:
+    pass
+
+
+class GffWriter:
+    pass
+
+
+if __name__ == "__main__":
+    pass
