4d69e6f742c5fbcc33ca67278687df317999c724
Christopher Neely <christopher.neely1200@gmail.com>
2020-09-09 17:18:13 -0700

    Combine logic from old Makefile into into install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,3 +7,8 @@ wget https://mmseqs.com/metaeuk/metaeuk-linux-sse41.tar.gz
 tar "xzf" metaeuk-linux-sse41.tar.gz && rm metaeuk-linux-sse41.tar.gz
 mv metaeuk/bin/metaeuk ./_metaeuk
 rm -r metaeuk/ && mv _metaeuk metaeuk
+python -m compileall . > /dev/null
+mkdir -p bin
+ln -srf EukMetaSanity/scripts/*.py bin/
+ln -srf EukMetaSanity/scripts/*.sh bin/
+pip install -r requirements.txt
