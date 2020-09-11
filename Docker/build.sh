c728780a35b0a42b961addd7de8d45beb1546a74
Christopher Neely <christopher.neely1200@gmail.com>
2020-09-11 15:22:00 -0700

    Begin extending tutorial; Make braker use existing genemark hints; update pipeline image

--- Docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ~/BioProjects/EukMetaSanity|| return
-docker build -t eukmetasanity:v0.1.0 .
-docker tag eukmetasanity:v0.1.0 cjneely10/eukmetasanity:v0.1.0
-#docker push cjneely10/eukmetasanity:v0.1.0
