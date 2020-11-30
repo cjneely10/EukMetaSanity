bb6c5f8b15f2f15bedc3957b2ddf2cab23a7fa22
Christopher Neely <christopher.neely1200@gmail.com>
2020-11-29 19:56:14 -0800

    Only output root and final members, small bug outputting everything

--- EukMetaSanity/tasks/base/task_manager.py
+++ EukMetaSanity/tasks/base/task_manager.py
@@ -76,6 +76,6 @@ class TaskManager:
         del output_dicts["default_factory"]
         # Also store original files
         for in_prefix, in_data in zip(self.input_prefixes, self.input_files):
-            output_dicts[in_prefix]["root"] = in_data["root"]
+            output_dicts[in_prefix].update(in_data["root"])
         json.dump(dict(output_dicts), _paths_output_file, sort_keys=True)
         _paths_output_file.close()
