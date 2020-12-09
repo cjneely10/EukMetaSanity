c3efc6b29df2032f91cc9bf291aa8fbac6796dfa
Christopher Neely <christopher.neely1200@gmail.com>
2020-12-08 20:08:47 -0800

    Add cargo build dependency

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,5 +6,7 @@ wget https://mmseqs.com/metaeuk/metaeuk-linux-sse41.tar.gz
 tar "xzf" metaeuk-linux-sse41.tar.gz && rm metaeuk-linux-sse41.tar.gz
 mv metaeuk/bin/metaeuk ./_metaeuk
 rm -r metaeuk/ && mv _metaeuk metaeuk && cd - || return
+cd EukMetaSanity/scripts/final-annotations && cargo build --release && cd - || return
 ln -srf EukMetaSanity/scripts/*.py bin/
 ln -srf EukMetaSanity/scripts/*.sh bin/
+ln -srf EukMetaSanity/scripts/final-annotations/target/release/final-annotations bin/
