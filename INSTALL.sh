68c1afdaf56f61499a4e65d978dfebf5a4764c94
Christopher Neely <christopher.neely1200@gmail.com>
2020-12-10 15:55:27 -0800

    Remove .sh copy in install step

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,4 +7,3 @@ tar "xzf" metaeuk-linux-sse41.tar.gz && rm metaeuk-linux-sse41.tar.gz
 mv metaeuk/bin/metaeuk ./_metaeuk
 rm -r metaeuk/ && mv _metaeuk metaeuk && cd - || return
 ln -srf EukMetaSanity/scripts/*.py bin/
-ln -srf EukMetaSanity/scripts/*.sh bin/
