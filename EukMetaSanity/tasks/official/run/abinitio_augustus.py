c62798c91f090b14c3124a9c9cde6d713daa41b4
Christopher Neely <christopher.neely1200@gmail.com>
2020-12-13 23:15:37 -0800

    Calling class wrappers complete

---
+++ EukMetaSanity/tasks/official/run/abinitio_augustus.py
@@ -0,0 +1,26 @@
+import os
+from EukMetaSanity import Task, TaskList, program_catch
+
+
+class AbInitioAugustusIter(TaskList):
+    name = "abinitio.augustus"
+    requires = ["taxonomy"]
+    depends = ["augustus"]
+    
+    class AbInitioAugustus(Task):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.output = {
+                "final": ["augustus.ab-gff3"]
+            }
+            
+        @program_catch
+        def run(self):
+            pass
+            
+    def __init__(self, *args, **kwargs):
+        super().__init__(AbInitioAugustusIter.AbInitioAugustus, AbInitioAugustusIter.name, *args, **kwargs)
+
+
+if __name__ == "__main_":
+    pass
