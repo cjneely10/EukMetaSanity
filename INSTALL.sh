761fdbfaf601a9d98da3c407e1c0758d3f88979a
Christopher Neely <christopher.neely1200@gmail.com>
2020-12-26 16:10:20 -0800

    Update INSTALL script for new api and scripts directories

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,3 +7,4 @@ tar "xzf" metaeuk-linux-sse41.tar.gz && rm metaeuk-linux-sse41.tar.gz
 mv metaeuk/bin/metaeuk ./_metaeuk
 rm -r metaeuk/ && mv _metaeuk metaeuk && cd - || return
 ln -srf EukMetaSanity/tasks/scripts/*.py bin/
+ln -srf EukMetaSanity/api/*.py bin/
