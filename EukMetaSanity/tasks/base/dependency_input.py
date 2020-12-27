88aa49e304c535ae0ddaffda04075cf5e9646381
Christopher Neely <christopher.neely1200@gmail.com>
2020-12-26 19:05:51 -0800

    Begin adding ability to pass data variables to downstream tasks

--- EukMetaSanity/tasks/base/dependency_input.py
+++ EukMetaSanity/tasks/base/dependency_input.py
@@ -1,17 +1,19 @@
-from typing import Tuple
+from typing import Dict
 
 
 class DependencyInput:
     """ Class handles parsing input for dependency requirements
 
     """
-    def __init__(self, name: str, primary_input: str = "root"):
+    def __init__(self, name: str, additional_data: Dict[str, object] = {}, primary_input: str = "root"):
         """ Create object
 
         :param name: Name of dependency, example mmseqs.createdb
+        :param additional_data: Dict of additional data to pass to dependency
         :param primary_input: Input to use, default is root.fna
         """
         self._name = name
+        self._args = additional_data
         self._input = primary_input
 
     @property
@@ -29,3 +31,11 @@ class DependencyInput:
         :return: Parsed input object string as tuple
         """
         return self._input
+
+    @property
+    def args(self) -> Dict[str, object]:
+        """ Get additional passed data to dependency
+
+        :return:
+        """
+        return self._args
