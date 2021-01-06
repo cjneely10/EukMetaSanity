788a90b116601f85ef35e93eb7d6a35572f7a663
Christopher Neely <christopher.neely1200@gmail.com>
2021-01-06 09:54:41 -0800

    Add base braker logic

--- EukMetaSanity/tasks/dependencies/braker/braker.py
+++ EukMetaSanity/tasks/dependencies/braker/braker.py
@@ -24,7 +24,7 @@ class BrakerIter(TaskList):
 
     """
     name = "braker"
-    requires = ["mapping", "taxonomy"]
+    requires = ["mapping", "taxonomy", "repeats"]
     depends = [DependencyInput("mmseqs.filtertaxseqdb")]
 
     class Braker(Task):
@@ -59,6 +59,57 @@ class BrakerIter(TaskList):
                 if "fungi" in tax[0]:
                     _tax = ["--fungus"]
                 _fasta_output = self.input["mmseqs.filtertaxseqdb"]["fastas"]
+                _added = []
+                gffread = self.local["gffread"]
+                if "--skipGeneMark-ES" in self.added_flags:
+                    # Output CDS and prot fasta
+                    _gtf = os.path.join(self.wdir, self.record_id + ".gtf")
+                    self.single(
+                        gffread[self.input["root"]["nr-gff3"]] > _gtf
+                    )
+                    _added = ["--geneMarkGtf=%s" % _gtf]
+                if not os.path.exists(os.path.join(self.wdir, "GeneMark-ET", "genemark.gtf")) or \
+                        not os.path.exists(os.path.join(self.wdir, "augustus.hints.gtf")):
+                    self.parallel(
+                        self.program[
+                            "--cores=%s" % str(self.threads),
+                            "--genome=%s" % self.input["repeats"]["fna"],
+                            (*bams),
+                            "--prot_seq=%s" % _fasta_output,
+                            "--workingdir=%s" % self.wdir,
+                            (*_tax),
+                            "--species=%s" % self.record_id,
+                            (*self.added_flags),
+                            "--prg=exonerate",
+                            (*_added)
+                        ]
+                    )
+                _files = [
+                    os.path.join(self.wdir, "GeneMark-ET", "genemark.gtf"),
+                    os.path.join(self.wdir, "augustus.hints.gtf"),
+                ]
+                _out = os.path.join(self.wdir, "braker.gtf")
+                if "--gff3" in self.added_flags:
+                    _files = [
+                        os.path.join(self.wdir, "GeneMark-ET", "genemark.gff3"),
+                        os.path.join(self.wdir, "augustus.hints.gff3"),
+                    ]
+                    _out = os.path.join(self.wdir, "braker.gff3"),
+                # Merge final results if final output failed
+                if not os.path.exists(_out):
+                    self.single(
+                        self.local[self.config["program_gffread"]][
+                            (*_files),
+                            "--merge", "-G", "-S",
+                            "-o", os.path.join(self.wdir, self.record_id + ".gff3"),
+                            "-g", self.input[2],
+                            "-x", os.path.join(self.wdir, self.record_id + ".cds.fna"),
+                            "-y", os.path.join(self.wdir, self.record_id + ".faa"),
+                        ]
+                    )
+                # Else just keep
+                else:
+                    self.output["nr_gff3"] = _out
 
         def _get_bams(self):
             """ Get list of bam files associated with input as a list in braker-input format
