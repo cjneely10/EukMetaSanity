f215790f2637dfb9318f659558a8510aff4ec172
Christopher Neely <christopher.neely1200@gmail.com>
2021-01-15 21:57:30 -0800

    Small UI edit

--- EukMetaSanity/tasks/base/input_manager.py
+++ EukMetaSanity/tasks/base/input_manager.py
@@ -130,12 +130,13 @@ class InputManager:
         """
         input_keys = {key for data_dict in self.input_files for key in data_dict.keys() if key != ConfigManager.ROOT}
         return "\n".join((
-            (colors.reset | "") +
-            (colors.bold & colors.blue | f"\nNumber of unique records gathered: {len(self.input_prefixes)}"),
-            colors.reset | "",
+
             colors.bold & colors.blue | "\n".join((
                 f"{record_id}: {len(self.input_files[i][ConfigManager.ROOT])} file(s) from command line, "
                 f"{sum([len(self.input_files[i][key]) for key in input_keys])} file(s) from internal pipelines"
                 for i, record_id in enumerate(self.input_prefixes)
-            ))
+            )),
+            colors.reset | "",
+            (colors.reset | "") +
+            (colors.bold & colors.blue | f"\nNumber of unique records gathered: {len(self.input_prefixes)}"),
         ))
