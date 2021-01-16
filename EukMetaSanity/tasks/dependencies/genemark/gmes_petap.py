5d6f483a2317ce751caf9989450fea1a19eb54a5
Christopher Neely <christopher.neely1200@gmail.com>
2021-01-16 12:28:59 -0800

    Continue to catch dependency_input pre-refactor issues

--- EukMetaSanity/tasks/dependencies/genemark/gmes_petap.py
+++ EukMetaSanity/tasks/dependencies/genemark/gmes_petap.py
@@ -23,7 +23,7 @@ class GeneMarkPetapIter(TaskList):
                            "--evidence", str(self.input["gmes.prothint"]["evidence"])]
             script = self.create_script(
                 self.program[
-                    "--sequence", str(self.dependency_input["fna"]),
+                    "--sequence", str(self.dependency_input["fasta"]),
                     (*ev_vals),
                     "--cores", self.threads, (*self.added_flags),
                     ("--fungus"
