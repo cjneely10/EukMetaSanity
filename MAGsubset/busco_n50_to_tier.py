54673db51074e85beca2fb3b6d32bb491d2588f3
Christopher Neely <christopher.neely1200@gmail.com>
2021-01-24 17:55:25 -0800

    Add tier by busco/n50 script

---
+++ MAGsubset/busco_n50_to_tier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+from plumbum import cli
+
+
+class BuscoN50TierGrapher(cli.Application):
+
+
+    def main(self):
+        pass
+
+
+if __name__ == "__main__":
+    BuscoN50TierGrapher.run()
