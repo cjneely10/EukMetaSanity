69dabc5d33d79c9ccd0623c5939638c1c3de2ae2
Christopher Neely <christopher.neely1200@gmail.com>
2021-08-27 12:14:42 -0700

    Automate uid setting

--- EukMetaSanity/data/config_generation_utils.py
+++ EukMetaSanity/data/config_generation_utils.py
@@ -5,16 +5,19 @@ import glob
 import os
 from pathlib import Path
 
+from plumbum import local
+
 
 def update_config_files(data_path: str, eukms_bin_dir: str):
     """ Get all config files matching pattern *-config.* and replace the "/path/to" strings with their actual paths
 
     """
+    user_uid = local["whoami"]()
     for config_file in glob.glob(os.path.join(os.path.dirname(eukms_bin_dir), "*", "*", "*-config.*")):
         updated_config_dir = os.path.dirname(os.path.dirname(config_file))
         new_config_file = open(os.path.join(updated_config_dir, os.path.basename(config_file)), "w")
         original_file_ptr = open(config_file, "r")
         for line in original_file_ptr:
-            line = line.replace("/path/to", str(Path(data_path).resolve()))
+            line = line.replace("uid", user_uid).replace("/path/to", str(Path(data_path).resolve()))
             new_config_file.write(line)
         new_config_file.close()
